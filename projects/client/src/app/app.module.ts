4043f3a348ff6ee8e946a335caf14c83937f6c1e
George Spanos <gespa11019@gmail.com>
2021-02-23 00:20:21 +0200

    Initial Pages

--- projects/client/src/app/app.module.ts
+++ projects/client/src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { MaterialModule } from '@boilerplate/shared';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/shared/src/lib/material.module.ts
+++ projects/shared/src/lib/material.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from "@angular/core";
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const materialModule = [
-    MatSidenavModule
+    MatButtonModule, MatCardModule,
+    MatFormFieldModule, MatInputModule,
+    MatSnackBarModule, MatSidenavModule,
+    MatListModule, MatIconModule,
+    MatToolbarModule, MatSelectModule,
+    MatRadioModule, MatCheckboxModule,
+    MatTableModule, MatDialogModule,
+    MatAutocompleteModule, MatSlideToggleModule,
+    MatTabsModule, MatExpansionModule,
+    MatMenuModule, MatPaginatorModule,
+    MatSortModule, MatBottomSheetModule
 ]
 
 @NgModule({
